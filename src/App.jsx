57e631fe56451ee9e99cc4fec9b32b28359406d3
Rakshit6722 <rakshitchauhan435@gmail.com>
2025-02-13 23:39:38 +0530

    navbar complete

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Highlights from './components/Highlights'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      <Highlights/>
+    </div>
+  )
+}
+
+export default App
